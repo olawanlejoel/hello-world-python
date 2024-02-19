417617b87dd238290c8507b8eddeb4120555956f
olawanlejoel <info4tobest2@gmail.com>
2024-02-19 13:38:20 +0100

    testing stuffs

--- disk_space_checker.py
+++ disk_space_checker.py
@@ -1,4 +1,7 @@
 import psutil
+import os
+import json
+import requests
 
 def get_disk_usage():
  #Gets the disk usage of persistent storage.
@@ -12,6 +15,45 @@ def get_disk_usage():
    "free": usage.free / (1024 ** 3)
  }
 
+def send_slack_alert(disk_usage_info):
+  endpoint = os.environ.get('SLACK_WEBHOOK_URI')
+
+  # Create a Slack message attachment
+  attachment = {
+    	"text": "Disk Space Usage",
+    	"fields": [
+        	{
+            	"title": "Used",
+            	"value": str(disk_usage_info['used']),
+            	"short": True
+        	},
+        	{
+            	"title": "Free",
+            	"value": str(disk_usage_info['free']),
+            	"short": True
+        	},
+        	{
+            	"title": "Total",
+            	"value": str(disk_usage_info['total']),
+            	"short": True
+        	}
+    	]
+	}
+ 
+  # Create the main message payload
+  payload = {
+	"attachments": [attachment]
+	}
+ 
+  # Convert the payload to JSON format
+  json_data = json.dumps(payload)
+ 
+  headers = {
+    	"Content-type": "application/json"
+	}
+ 
+  requests.post(endpoint, data=json_data, headers=headers)
+
 if __name__ == "__main__":
  disk_info = get_disk_usage()
 
@@ -21,3 +63,5 @@ if __name__ == "__main__":
 
  if(disk_info['free'] < threshold_level):
    print('Alert! Disk space is below the set threshold level.')
+
+send_slack_alert(disk_info)
